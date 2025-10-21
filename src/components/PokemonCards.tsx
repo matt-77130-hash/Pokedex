b9b5d59a9375b0b51bba74cdc9872f69a0554fdf
Matthieu Bergeron <Matt-77130@hotmail.fr>
2025-10-21 16:01:27 +0200

    Pokedex quête 3

--- src/components/PokemonCards.tsx
+++ src/components/PokemonCards.tsx
@@ -1,10 +1,40 @@
-function PokemonCards() {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
-        </figure>
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+function PokemonCard() {
+    const pokemon = pokemonList[0];
 
-    );
+    if (pokemon.imgSrc) {
+        return (
+            <figure>
+                <img src={pokemon.imgSrc} alt={pokemon.name} />
+                <figcaption>{pokemon.name}</figcaption>
+            </figure>
+
+
+        );
+    }
+    else {
+        return (
+            < figure >
+                <img src={pokemon.imgSrc} alt={"???"} />
+                <figcaption>{pokemon.name}</figcaption>
+            </figure >
+
+        )
+    };
 }
-export default PokemonCards;
+
+
+
+
+
+export default PokemonCard;
+
